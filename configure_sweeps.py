b315eeef883ba0ec9c09a858a4e81ffc223e8d82
StormMata <storm.mata097@gmail.com>
2024-10-25 16:24:02 -0400

    update config

--- configure_sweeps.py
+++ configure_sweeps.py
@@ -60,9 +60,10 @@ is_mac = platform.system() == "Darwin"
 # Create all combinations
 combinations = list(itertools.product(shear, veer))
 
-filtered_combinations = [pair for pair in combinations if pair not in excluded_pairs]
+filtered_combinations  = [pair for pair in combinations if pair not in excluded_pairs]
+formatted_combinations = [f"r's{pair[0]}_v{pair[1]}'" for pair in filtered_combinations]
 
-print(f'FILTERED COMBINATIONS: {filtered_combinations}')
+print(f'FILTERED COMBINATIONS: {formatted_combinations}')
 
 # Helper function to format the directory name without negative signs
 def format_value(val):
