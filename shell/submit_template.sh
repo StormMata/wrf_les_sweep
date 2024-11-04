8f4f23e11cc5bd4af22d9b6e182cd173ce665f40
StormMata <storm.mata097@gmail.com>
2024-11-04 14:23:19 -0500

    update iea10mw

--- shell/submit_template.sh
+++ shell/submit_template.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks=64
+#SBATCH --ntasks=72
 #SBATCH --time=36:00:00
 #SBATCH -J job_name
-##SBATCH -A ees240114
-#SBATCH -A atm170028
+#SBATCH -A ees240114
+##SBATCH -A atm170028
 #SBATCH -p wholenode
 #SBATCH --mail-user=storm@mit.edu
 #SBATCH --mail-type=all
@@ -16,6 +16,8 @@ source "export_libs_load_modules.sh"
 MPIRUN=$(which mpirun)
 export OPENMPI_ROOT=$(dirname ${MPIRUN} | sed -e 's_/bin__g')
 MPI_OPTS="--map-by L3cache -x OMPI_MCA_memory_linux_disable=1 -x MALLOC_MMAP_MAX_=0 -x MALLOC_TRIM_THRESHOLD_=-1 --mca mpi_paffinity_alone 1 --mca pml ucx --mca btl ^vader,tcp,openib,sm,self"
+
+ulimit -s unlimited
 
 echo INFO
 ompi_info
